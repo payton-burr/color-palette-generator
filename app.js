5d1591c781a3a154427e62c2fe8dd8c9b5674aa7
Payton Burr <36023709+payton-burr@users.noreply.github.com>
2020-06-26 15:25:41 -0600

    JS colorized each slider accordingly

--- app.js
+++ app.js
@@ -23,6 +23,14 @@ function randomColors() {
     div.style.backgroundColor = randomColor;
     hexText.innerText = randomColor;
     checkContrast(randomColor, hexText);
+
+    const color = chroma(randomColor);
+    const sliders = div.querySelectorAll('.sliders input');
+    const hue = sliders[0];
+    const brightness = sliders[1];
+    const saturation = sliders[2];
+
+    colorizeSliders(color, hue, brightness, saturation);
   });
 }
 
@@ -34,6 +42,30 @@ function checkContrast(color, text) {
   } else {
     text.style.color = '#ffffff';
   }
+}
+
+function colorizeSliders(color, hue, brightness, saturation) {
+  // Saturation
+  const noSat = color.set('hsl.s', 0);
+  const maxSat = color.set('hsl.s', 1);
+  const scaleSaturation = chroma.scale([noSat, color, maxSat]);
+  // Brightness
+  const defaultBrightness = color.set('hsl.l', 0.5);
+  const scaleBrightness = chroma.scale(['black', defaultBrightness, 'white']);
+
+  brightness.style.backgroundImage = `linear-gradient(to right, ${scaleBrightness(0)},
+                                                                ${scaleBrightness(0.5)},
+                                                                ${scaleBrightness(1)})`;
+
+  saturation.style.backgroundImage = `linear-gradient(to right, ${scaleSaturation(0)}, ${scaleSaturation(1)})`;
+
+  hue.style.backgroundImage = `linear-gradient(to right, rgb(255, 0, 0),
+                                                         rgb(255,255 ,0),
+                                                         rgb(0, 255, 0),
+                                                         rgb(0, 255, 255),
+                                                         rgb(0,0,255),
+                                                         rgb(255,0,255),
+                                                         rgb(255,0,0))`;
 }
 
 randomColors();
